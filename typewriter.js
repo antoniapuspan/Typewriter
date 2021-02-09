1f9e8def48080372755b803fbcdb1fdbabcd44f1
antoniapuspan <65085166+antoniapuspan@users.noreply.github.com>
2021-02-09 23:49:41 +0100

    set interval function to run 3 seconds

--- typewriter.js
+++ typewriter.js
@@ -1,7 +1,16 @@
 "use strict";
 //read text
 let myElement =  document.querySelector(".typewritten");
+
+setInterval(function () {
+    typeScript(myElement)
+}, 3000)
 //created variable to show as null
+function typeScript(n) {
 let myText = myElement.innerHTML;
-myElement.innerHTML = '';
+let toShow = "";
+myElement.innerHTML = toShow;
 let i = 0;
+
+
+}
