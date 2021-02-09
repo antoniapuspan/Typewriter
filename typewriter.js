cc6e31c51b2ea61ad66c3d50de13486141d1eb0a
antoniapuspan <65085166+antoniapuspan@users.noreply.github.com>
2021-02-09 23:57:51 +0100

    set interval for characters

--- typewriter.js
+++ typewriter.js
@@ -1,18 +1,25 @@
 "use strict";
 //read text
-let myElement =  document.querySelector(".typewritten");
+let myElement = document.querySelector(".typewritten");
 
 setInterval(function () {
     typeScript(myElement)
-}, 3000)
+}, 8000)
 //created variable to show as null
 function typeScript(n) {
-let myText = n.innerHTML;
-let toShow = "";
-myElement.innerHTML = toShow;
-let i = 0;
+    let myText = n.innerHTML;
+    let toShow = "";
+    myElement.innerHTML = toShow;
+    let i = 0;
 
-
+    let myInt = setInterval(function () {
+        toShow = toShow + myText[i];
+        n.innerHTML = toShow;
+        i++;
+        if (i == myText.length) {
+            clearInterval(myInt);
+        }
+    }, 200)
 
 
 }
