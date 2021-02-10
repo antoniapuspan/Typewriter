b9de4b62195b476b3ed5adc6c756550a83e3e56b
antoniapuspan <65085166+antoniapuspan@users.noreply.github.com>
2021-02-11 00:46:11 +0100

    correction

--- typewriter.js
+++ typewriter.js
@@ -1,10 +1,11 @@
 "use strict";
 //read text
 let myElement = document.querySelector(".typewritten");
-let keyAudio = document.getElementById("typekey1");
+let keyAudio1 = document.getElementById("typekey1");
+let keyAudio2 = document.getElementById("typekey2");
 let spaceAudio = document.getElementById("typespace");
 let lastKey = document.getElementById("typelast");
-
+let keySounds = [keyAudio1, keyAudio2];
 typeScript(myElement);
 setInterval(function () {
     typeScript(myElement)
@@ -18,23 +19,21 @@ function typeScript(n) {
     let myInt = setInterval(function () {
         toShow = toShow + myText[i];
         n.innerHTML = toShow;
-        i++;
-
-
         if (myText[i] == " ") {
             playSound(spaceAudio);
         } else {
-            playSound(keyAudio);
+            let random = Math.floor(Math.random() * 2)
+            console.log(random);
 
+            playSound(keySounds[random]);
         }
+        i++;
 
         if (i == myText.length) {
             playSound(lastKey);
             clearInterval(myInt);
         }
     }, 200)
-
-
 }
 
 function playSound(sound) {
