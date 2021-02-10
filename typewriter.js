255bb20944f754091a4d8a62aee21ca15d0f68f2
antoniapuspan <65085166+antoniapuspan@users.noreply.github.com>
2021-02-11 00:16:38 +0100

    added sounds

--- typewriter.js
+++ typewriter.js
@@ -1,7 +1,11 @@
 "use strict";
 //read text
 let myElement = document.querySelector(".typewritten");
+let keyAudio = document.getElementById("typekey1");
+let spaceAudio = document.getElementById("typespace");
+let lastKey = document.getElementById("typelast");
 
+typeScript(myElement);
 setInterval(function () {
     typeScript(myElement)
 }, 10000)
@@ -11,15 +15,31 @@ function typeScript(n) {
     let toShow = "";
     myElement.innerHTML = toShow;
     let i = 0;
-
     let myInt = setInterval(function () {
         toShow = toShow + myText[i];
         n.innerHTML = toShow;
         i++;
+
+
+        if (myText[i] == " ") {
+            playSound(spaceAudio);
+        } else {
+            playSound(keyAudio);
+
+        }
+
         if (i == myText.length) {
+            playSound(lastKey);
             clearInterval(myInt);
         }
     }, 200)
 
+
+}
+
+function playSound(sound) {
+    sound.pause();
+    sound.currentTime = 0;
+    sound.play();
 
 }
